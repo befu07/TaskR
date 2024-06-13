92a0c7f70a86e802bfd2323e5d54bcdc1bb696af
Bernhard Futtera <ITN276497@qualifizierung.at>
2024-06-13 13:32:35 +0200

    skriptfile dotnet scaffolding

---
+++ TaskR/scaffolding.bat
@@ -0,0 +1,10 @@
+﻿@echo off
+rem Pfad von Skriptdatei in variable schreiben und ausgeben
+set scriptDir=%~dp0
+echo aktueller Projekt-Pfad: %scriptDir%
+
+rem scaffolding befehl ausführen 
+echo befehl ist auskommentiert
+echo dotnet ef dbcontext scaffold name="AppDb" Microsoft.EntityFrameworkCore.SqlServer -o Data -f
+
+pause
